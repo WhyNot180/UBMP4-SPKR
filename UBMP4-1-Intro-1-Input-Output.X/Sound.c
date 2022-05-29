e9f086473ff3460aa973b80f736bbbca094d96d5
Bence Szucs <bence.szucs200625@gmail.com>
2022-05-28 22:15:12 -0400

    re-adds the structure domain

--- UBMP4-1-Intro-1-Input-Output.X/Sound.c
+++ UBMP4-1-Intro-1-Input-Output.X/Sound.c
@@ -130,7 +130,7 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
     do {
         //note: the effect counter is effectively a divisor of the waveform (i.e. effect = 1 waveform = 50%, effect = 2 waveform = 25%)
         // try finding other relationships with other time domains
-        BEEPER = outputs[0][0] & outputs[0][1] & outputs[0][2];
+        BEEPER = outputs[0][0] & outputs[0][1] & outputs[0][2] & outputs[0][3];
         LED3 = outputs[0][0] & outputs[0][1] & outputs[0][2];
         if (pitch[0]-- == 0) {
             pitch[0] = period;
@@ -150,7 +150,7 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
             //     }
             // }
         }
-        BEEPER = outputs[1][0] & outputs[1][1] & outputs[1][2];
+        BEEPER = outputs[1][0] & outputs[1][1] & outputs[1][2] & outputs[1][3];
         LED3 = outputs[1][0] & outputs[1][1] & outputs[1][2];
         if (pitch[1]-- == 0) {
             pitch[1] = period2;
@@ -166,7 +166,7 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
             //     }
             // }
         }
-        BEEPER = outputs[2][0] & outputs[2][1] & outputs[2][2];
+        BEEPER = outputs[2][0] & outputs[2][1] & outputs[2][2] & outputs[2][3];
         LED3 = outputs[2][0] & outputs[2][1] & outputs[2][2];
         if (pitch[2]-- == 0) {
             pitch[2] = period3;
@@ -191,6 +191,10 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
                 if (rhythms[0]-- == 0) {
                     rhythms[0] = coreRhythm;
                     outputs[0][2] ^= 1;
+                    if (structures[0]-- == 0) {
+                        structures[0] = coreStructure;
+                        outputs[0][3] ^= 1;
+                    }
                 }
             }
 
@@ -200,6 +204,10 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
                 if (rhythms[1]-- == 0) {
                     rhythms[1] = coreRhythm;
                     outputs[1][2] ^= 1;
+                    if (structures[1]-- == 0) {
+                        structures[1] = coreStructure;
+                        outputs[1][3] ^= 1;
+                    }
                 }
             }
 
@@ -209,6 +217,10 @@ void _makeSound(unsigned long cycles, unsigned long period, bool silent,
                 if (rhythms[2]-- == 0) {
                     rhythms[2] = coreRhythm;
                     outputs[2][2] ^= 1;
+                    if (structures[2]-- == 0) {
+                        structures[2] = coreStructure;
+                        outputs[2][3] ^= 1;
+                    }
                 }
             }
         }
